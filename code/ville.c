c2deb1560becc0fdfad8941f4e35f1a3aea458b9
Martin <Equynoxe>
2020-05-07 15:48:51 +0200

    ville.c code + ville.h

---
+++ code/ville.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+#include "ville.h"
+
+struct Ville_t{
+    char nom[60];
+    int x;
+    int y;
+    char specialite[100];
+};
+
+Ville *creer_ville(char *nom, int x, int y){
+    Ville ville = malloc(sizeof(Ville));
+    ville->nom = nom;
+    ville->x = x;
+    ville->y = y;
+
+    return ville;
+}
+
+int get_x_ville(Ville *ville){
+    assert(ville != NULL);
+    return ville->x;
+}
+
+int get_y_ville(Ville *ville){
+    assert(ville != NULL);
+    return ville->y;
+}
+
+char *get_nom_ville(Ville *ville){
+    assert(ville!=NULL);
+    return ville->nom;
+}
+
+char *get_specialite_ville(Ville *ville){
+    assert(ville != NULL);
+    return ville->specialite;
+}
+
+void set_specialite_ville(Ville *ville, char *specialite){
+    ville->specialite = specialite;
+}
+
+double distance_entre_2_villes(Ville *ville1, Ville *ville2){
+    return math.sqrt(pow(ville2->x - ville1->x, 2) + pow(ville2->y - ville1->y, 2));
+}
---
+++ code/ville.h
@@ -0,0 +1,20 @@
+#ifndef _VILLE_
+#define _VILLE_
+
+typedef struct Ville_t Ville;
+
+Ville *creer_ville(char *nom, int x, int y);
+
+int get_x_ville(Ville *ville);
+
+int get_y_ville(Ville *ville);
+
+char *get_nom_ville(Ville *ville);
+
+char *get_specialite_ville(Ville *ville);
+
+void set_specialite_ville(Ville *ville, char *specialite);
+
+double distance_entre_2_villes(Ville *ville1, Ville *ville2);
+
+#endif
